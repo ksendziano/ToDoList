e054acaada2e760b5259d78f1242883ad7ca703b
ksendziano <45664234+ksendziano@users.noreply.github.com>
2020-08-11 17:36:12 +0700

    Update TaskController.php

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,10 +6,10 @@ use App\Board;
 use App\Http\Requests;
 use App\Repositories\TaskRepository;
 use App\Task;
-use Carbon\Carbon;
-use App\User;
+use Carbon\Carbon;# не используется
+use App\User;#-||-
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Controller;#-||-
 
 class TaskController extends Controller
 {
@@ -27,13 +27,13 @@ class TaskController extends Controller
     public function index(Request $request,$board_id)
     {
         $board = Board::find($board_id);
-        $tasks = $board->tasks;
+        $tasks = $board->tasks; # снова локальная переменная названа как поле класса, не круто
         if (!policy(Task::class)->index($request->user(), $board)) {
             abort(403);
         }
         return response()->json($tasks,200);
     }
-    public function create(Request $request,$board_id)
+    public function create(Request $request,$board_id) # снова board_id не в теле запроса, ну и так везде тут
     {
         $board = Board::find($board_id);
         if (!policy(Task::class)->create($request->user(), $board)) {
@@ -53,7 +53,7 @@ class TaskController extends Controller
             'scheduled_date' => 'required'
         ]);
         $board->user->tasks()->create([
-            'name' => $request->name,
+            'name' => $request->name, # title
             'board_id'=>$board_id,
             'description'=>$request->description,
             'scheduled_date'=>$request->scheduled_date,
@@ -63,7 +63,7 @@ class TaskController extends Controller
         $task=Task::where('name',$request->name);
         return response()->json($task,200);
     }
-    public function copy(Request $request,$board_id,$task_id)
+    public function copy(Request $request,$board_id,$task_id)# board_id не используется, зачем-то еще task_id передается, все это дело можно передавать в $request
     {
         $task=Task::find($task_id);
         $this ->authorize('action',$task);
@@ -77,15 +77,15 @@ class TaskController extends Controller
         ]);
         return response(null, 200);
     }
-    public function move(Request $request,$board_id,$task_id)
+    public function move(Request $request,$board_id,$task_id)#-||-
     {
         $task=Task::find($task_id);
         $this ->authorize('action',$task);
-        $task->board_id = $request->to_board_id;
+        $task->board_id = $request->to_board_id; # продумывай нейминг, пожалуйста to_board_id это никуда не годится
         $task->save();
         return response(null, 200);
     }
-    public function edit(Request $request,$board_id, $task_id)
+    public function edit(Request $request,$board_id, $task_id)# $request вообще не используется
     {
         $task=Task::find($task_id);
         $this ->authorize('action',$task);
@@ -94,7 +94,7 @@ class TaskController extends Controller
             'board_id'=>$board_id,
         ]);
     }
-    public function update(Request $request,$board_id,$task_id)
+    public function update(Request $request,$board_id,$task_id) # уже говорил
     {
         $task = Task::find($task_id);
         $this ->authorize('action',$task);
@@ -103,7 +103,7 @@ class TaskController extends Controller
             'description' => $request->description,
             'scheduled_date' => $request->scheduled_date,
         ]);
-        if($request->status==1)
+        if($request->status==1) # что это значит? status ==1?
         {
             $task->update([
                 'status'=> 1,
@@ -112,7 +112,7 @@ class TaskController extends Controller
         }
         return response()->json($task, 200);
     }
-    public function destroy(Request $request,$board_id, $task_id)
+    public function destroy(Request $request,$board_id, $task_id) # тут используется только task_id
     {
         $task=Task::find($task_id);
         $this ->authorize('action',$task);
